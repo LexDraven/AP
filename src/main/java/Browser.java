fd0af4fc35e313854fd376d8bfaba331cd9e307f
LexDraven <Lexman2@ya.ru>
2017-05-14 21:28:44 +0500

    add some tests

--- src/main/java/Browser.java
+++ src/main/java/Browser.java
@@ -1,8 +1,13 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.CookieSpecs;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -10,13 +15,10 @@ import java.util.concurrent.TimeUnit;
 public class Browser {
     private WebDriver webDriver;
     private int timeToWait = 3;
+    private String mainPageUrl;
 
-    public Browser(int timeToWait) {
-        this.timeToWait = timeToWait;
-        init();
-    }
-
-    public Browser() {
+    public Browser(String mainPageUrl) {
+        this.mainPageUrl = mainPageUrl;
         init();
     }
 
@@ -65,5 +67,116 @@ public class Browser {
         } catch (InterruptedException e) {
 
         }
+    }
+
+    private  boolean isLinkAlive (String URL){
+        HttpResponse response;
+        try {
+            RequestConfig config= RequestConfig.custom().setCookieSpec(CookieSpecs.IGNORE_COOKIES).setSocketTimeout(15000).setConnectTimeout(10000).setConnectionRequestTimeout(10000).build();
+            HttpClient client= HttpClientBuilder.create().setDefaultRequestConfig(config).build();
+            HttpGet request = new HttpGet(URL);
+            response = client.execute(request);
+        } catch (Exception e) {
+            System.err.println(URL + " error " + e.getLocalizedMessage());
+            return false;
+        }
+        return  response.getStatusLine().getStatusCode()==200;
+    }
+
+    public boolean allLinksOnPageAlive(){
+        List<WebElement> list = webDriver.findElements(By.tagName("a"));
+        boolean result=true;
+        for(WebElement webElement:list){
+            String url = webElement.getAttribute("href");
+            if (!url.startsWith("http")){
+               url=mainPageUrl+url;
+            }
+            if (!isLinkAlive(url)) {
+                System.out.println("Failed link: "+url);
+                result = false;
+            }
+        }
+        return result;
+    }
+
+    public boolean chooseTopLineElement(String name){
+        if (!isElementPresent(By.className("top-line__nav"))) {
+            return false;
+        }
+        if (!isElementPresent(By.xpath("//a[contains(.,'"+name+"')]"))) {
+            return false;
+        }
+        return clickElement(By.xpath("//a[contains(.,'"+name+"')]"));
+    }
+
+    protected boolean clickElement(By locator) {
+        try {
+            webDriver.findElement(locator).click();
+            return true;
+        } catch (WebDriverException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    public String getCurrentUrl(){
+        return webDriver.getCurrentUrl();
+    }
+
+    public boolean changeLanguage (String newLanguage){
+        if (getChosenLanguage().equals(newLanguage)){
+            return true;
+        }
+        waitUntilVisible(By.cssSelector(".custom-select__active"),5);
+        if (!clickElement(By.cssSelector(".custom-select__active"))) {
+            return false;
+        }
+        clickElement(By.xpath("//span[text()='"+newLanguage+"']/parent::div"));
+        return waitUntilDisappear(By.cssSelector("custom-select__options"),5);
+    }
+
+    public String getChosenLanguage(){
+        if (!isElementPresent(By.cssSelector(".custom-select__active"))){
+            return "no language menu";
+        }
+        String text =(String) ((JavascriptExecutor)webDriver).executeScript("return document.querySelector('.custom-select__active .text').innerText");
+        return text;
+    }
+
+    protected boolean waitUntilExist(By locator, int time) { //ожидание появления всех элементов класса
+        return waitUntilConditions(locator, time, 0);
+    }
+
+    protected boolean waitUntilVisible(By locator, int time) { //ожидание видимости элемента
+        return waitUntilConditions(locator, time, 1);
+    }
+
+    protected boolean waitUntilClickable(By locator, int time) { //ожидание возможности нажатия
+        return waitUntilConditions(locator, time, 2);
+    }
+
+    protected boolean waitUntilDisappear(By locator, int time) { //ожидаем исчезновения элемента
+        return waitUntilConditions(locator, time, 3);
+    }
+
+    private boolean waitUntilConditions(By locator, int time, int type) {
+        try {
+            WebDriverWait wait = new WebDriverWait(webDriver, time);
+            if (type == 0) { //exist
+                wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(locator));
+            }
+            if (type == 1) { //visible
+                wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+            }
+            if (type == 2) { //clickable
+                wait.until(ExpectedConditions.elementToBeClickable(locator));
+            }
+            if (type == 3) { //disappear
+                wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+            }
+        } catch (TimeoutException ex) {
+            return false;
+        }
+        return true;
     }
 }
--- src/main/java/MainPageTest.java
+++ src/main/java/MainPageTest.java
@@ -1,3 +1,4 @@
+import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -10,14 +11,13 @@ public class MainPageTest {
 
     @BeforeSuite
     public void setUp() throws Exception {
-        browser = new Browser();
+        browser = new Browser(mainPageUrl);
         browser.goToURL(firstPageToSkipFlashUrl);
     }
 
     @BeforeMethod
     public void startPage(){
         browser.goToURL(mainPageUrl);
-        browser.waitForJQueryEnds();
     }
 
     @AfterSuite
@@ -26,11 +26,60 @@ public class MainPageTest {
     }
 
     @Test
-    public void openMainPage(){
-        browser.goToURL(mainPageUrl);
-        browser.pause(2);
-
+    public void openMainPageAndCheckAllLinks(){
+        Assert.assertTrue(browser.allLinksOnPageAlive());
     }
+
+    @Test
+    public void navigateToMedia(){
+        Assert.assertTrue(browser.chooseTopLineElement("Материалы"));
+        Assert.assertTrue(browser.getCurrentUrl().endsWith("/media/"));
+    }
+
+    @Test
+    public void navigateToTournaments(){
+        Assert.assertTrue(browser.chooseTopLineElement("Турниры"));
+        Assert.assertTrue(browser.getCurrentUrl().equals("http://tournament.tankionline.com/ru/"));
+    }
+
+    @Test
+    public void navigateToForum(){
+        Assert.assertTrue(browser.chooseTopLineElement("Форум"));
+        Assert.assertTrue(browser.getCurrentUrl().equals("http://ru.tankiforum.com/"));
+    }
+
+    @Test
+    public void navigateToWiki(){
+        Assert.assertTrue(browser.chooseTopLineElement("Вики"));
+        Assert.assertTrue(browser.getCurrentUrl().startsWith("http://ru.tankiwiki.com/"));
+    }
+
+    @Test
+    public void navigateToRatings(){
+        Assert.assertTrue(browser.chooseTopLineElement("Рейтинги"));
+        Assert.assertTrue(browser.getCurrentUrl().equals("http://ratings.tankionline.com/ru/"));
+    }
+
+    @Test
+    public void navigateToHelp(){
+        Assert.assertTrue(browser.chooseTopLineElement("Помощь"));
+        Assert.assertTrue(browser.getCurrentUrl().equals("http://help.tankionline.com/"));
+    }
+
+    @Test
+    public void checkChangingLanguage(){
+        Assert.assertTrue(browser.changeLanguage("English"));
+        browser.waitForJQueryEnds();
+        Assert.assertTrue(browser.getChosenLanguage().equals("English"),browser.getChosenLanguage());
+        Assert.assertTrue(browser.chooseTopLineElement("Game"));
+        Assert.assertTrue(browser.changeLanguage("Русский"));
+        Assert.assertTrue(browser.getChosenLanguage().equals("Русский"));
+    }
+
+
+
+
+
 
 
 
